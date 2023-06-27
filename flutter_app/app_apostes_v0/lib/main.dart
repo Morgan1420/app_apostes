6d30bac57f980a144de7ecfb307b81da1fa6453e
Morgan1420 <jan.moran.r@gmail.com>
2023-06-28 00:37:17 +0200

    initial_buttons_and_screens

--- flutter_app/app_apostes_v0/lib/main.dart
+++ flutter_app/app_apostes_v0/lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
+import 'createGameScreen.dart';
+import 'playGameScreen.dart';
+import 'selectGameScreen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(title: "aaaa", home: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            // make empty space
+            const Expanded(child: SizedBox()), // 10% of total height of screen
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SelectGameScreen()),
+                );
+              },
+              child: const Text('Select Game'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CreateGameScreen()),
+                );
+              },
+              child: const Text('Create Game'),
+            ),
+            const Expanded(child: SizedBox()),
+          ],
         ),
       ),
     );
---
+++ flutter_app/app_apostes_v0/lib/playGameScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PlayGameScreen extends StatelessWidget {
+  const PlayGameScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
